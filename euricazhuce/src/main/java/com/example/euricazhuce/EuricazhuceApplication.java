b585886a7816712e8ce68fbf998025561d384dd6
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2021-06-13 22:09:02 +0800

    。

--- euricazhuce/src/main/java/com/example/euricazhuce/EuricazhuceApplication.java
+++ euricazhuce/src/main/java/com/example/euricazhuce/EuricazhuceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //开启服务注册到注册中心
+@EnableEurekaClient //开启服务注册到注册中心 注册后，就可以使用其他服务或者被其他服务调用了
 public class EuricazhuceApplication {
 
     public static void main(String[] args) {
