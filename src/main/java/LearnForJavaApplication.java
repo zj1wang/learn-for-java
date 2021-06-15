b1fc6908462bdc2594561522244eedaee19b50a4
zhiheng.wang <zhiheng.wang@thoughtworks.com>
2021-06-15 09:28:20 +0800

    。

--- src/main/java/LearnForJavaApplication.java
+++ src/main/java/LearnForJavaApplication.java
@@ -1,11 +1,11 @@
-package learn.learnforjava;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableFeignClients//开启Feign功能
 public class LearnForJavaApplication {
 
 	public static void main(String[] args) {
